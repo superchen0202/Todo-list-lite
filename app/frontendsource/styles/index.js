c2ef50d07f54dd0d6822cf49676956f12e224f0e
superchen <chensuper0202@gmail.com>
2021-02-20 22:34:12 +0800

    font awesome installed and setting finished

--- app/frontendsource/styles/index.js
+++ app/frontendsource/styles/index.js
@@ -1,5 +1,9 @@
 import "./application";
 
+//tailwind css library
 import "tailwindcss/base";
 import "tailwindcss/components";
 import "tailwindcss/utilities";
+
+// font-awesome library
+import "@fortawesome/fontawesome-free/css/all.css";
